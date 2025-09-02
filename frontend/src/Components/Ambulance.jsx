c4c1004ef3c9544a168a9cac3f3c5dc8e9dc4970
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-09-02 19:47:39 +0530

    Ambulance Updated

--- frontend/src/Components/Ambulance.jsx
+++ frontend/src/Components/Ambulance.jsx
@@ -1,18 +1,3 @@
-// import React from "react";
-// import Lottie from "lottie-react";
-// import ambulanceAnimation from "../assets/ambulance.json";
-
-// const Ambulance = () => {
-//   return (
-//     <div style={{ maxWidth: "300px", margin: "0 auto" }}>
-//       <Lottie animationData={ambulanceAnimation} loop autoplay />
-//     </div>
-//   );
-// };
-
-// export default Ambulance;
-
-
 import React from "react";
 import Lottie from "lottie-react";
 import Heartrate from "../assets/Heartbeat Lottie Animation.json";
